9b4642046a71fbed29cb555360cdbd7c4d5b0eb8
noheechul <noheechul0511@naver.com>
2024-04-01 15:11:59 +0900

    :tada: new spring project

---
+++ src/main/java/com/kyungmin/lavanderia/LavanderiaApplication.java
@@ -0,0 +1,13 @@
+package com.kyungmin.lavanderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavanderiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LavanderiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyungmin/lavanderia/LavanderiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyungmin.lavanderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavanderiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
