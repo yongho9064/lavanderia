3d881d6e4b6da2e58a94bab3982e5ec95bd3c5af
gksktl111 <gksktl111@naver.com>
2024-04-24 22:37:55 +0900

    🩹 custom.d.ts 파일 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import 'tailwindcss/tailwind.css';
 
 function App() {
-
-
   return (
-      <div className="hi bg-red-500 p-4 h-800px">
-          <p className="hi bg-red-500 p-4">This is a styled div using Tailwind CSS.</p>
-      </div>
+    <div className='hi bg-red-500 p-4 h-800px'>
+      <p className='hi bg-red-500 p-4'>
+        This is a styled div using Tailwind CSS.
+      </p>
+    </div>
   );
 }
 
