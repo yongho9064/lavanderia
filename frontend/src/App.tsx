85f9755372fe33848f2f656bd0801bf3abcb94d9
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-04-26 16:00:41 +0900

    🩹 App .tsx수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,29 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import 'tailwindcss/tailwind.css';
-import Header from './Components/Common/Header';
-import Application from './pages/Application/Application';
-import Premium from './pages/Premium/Premium';
-import Community from './pages/Community/Community';
-import ServiceCenter from './pages/ServiceCenter/ServiceCenter';
-import Login from './pages/auth/Login';
-import Signup from './pages/Signup/Signup';
-
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "tailwindcss/tailwind.css";
+import Header from "./Components/Common/Header";
+import Application from "./pages/Application/Application";
+import Premium from "./pages/Premium/Premium";
+import Community from "./pages/Community/Community";
+import ServiceCenter from "./pages/ServiceCenter/ServiceCenter";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/Signup/Signup";
+import Home from "./pages/Home";
 
 function App() {
-    return (
-        <Routes>
-            <Route element={<Header/>}>
-                <Route path="application" element={<Application/>}/>
-                <Route path='premium' element={<Premium/>}/>
-                <Route path='community' element={<Community/>}/>
-                <Route path='servicecenter' element={<ServiceCenter/>}/>
-                <Route path='login' element={<Login/>}/>
-                <Route path='signup' element={<Signup/>}/>
-            </Route>
-            <Route path="/" element={<Home/>}/>
-        </Routes>
-    );
->>>>>>> 02b5b749204dc50fd4b3a8f873086a80fcd06f41
+  return (
+    <Routes>
+      <Route element={<Header />}>
+        <Route path="application" element={<Application />} />
+        <Route path="premium" element={<Premium />} />
+        <Route path="community" element={<Community />} />
+        <Route path="servicecenter" element={<ServiceCenter />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Route>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
