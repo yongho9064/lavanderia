c84a970bf24e1ea47f92ad66100a71f80c77dfdc
noheechul <noheechul0511@naver.com>
2024-04-28 21:15:57 +0900

    :sparkles: 전화번호 중복 검사, 이메일 인증 코드 기능 추가

--- src/main/java/com/kyungmin/lavanderia/member/data/repository/MemberRepository.java
+++ src/main/java/com/kyungmin/lavanderia/member/data/repository/MemberRepository.java
@@ -4,6 +4,12 @@ import com.kyungmin.lavanderia.member.data.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<MemberEntity,String> {
+
+    Optional<MemberEntity> findByMemberPhone(String phoneNumber);
+
+    Boolean existsByMemberPhone(String phoneNumber);
 }
--- src/main/java/com/kyungmin/lavanderia/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/kyungmin/lavanderia/member/service/impl/MemberServiceImpl.java
@@ -4,6 +4,11 @@ import com.kyungmin.lavanderia.member.data.dto.SignupDTO;
 import com.kyungmin.lavanderia.member.data.entity.MemberEntity;
 import com.kyungmin.lavanderia.member.data.repository.MemberRepository;
 import com.kyungmin.lavanderia.member.service.MemberService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,10 +22,14 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final JavaMailSender javaMailSender;
 
-    public MemberServiceImpl(MemberRepository memberRepository, BCryptPasswordEncoder passwordEncoder) {
+    public MemberServiceImpl(MemberRepository memberRepository, BCryptPasswordEncoder passwordEncoder, RedisTemplate<String, Object> redisTemplate, JavaMailSender javaMailSender) {
         this.memberRepository = memberRepository;
         this.passwordEncoder = passwordEncoder;
+        this.redisTemplate = redisTemplate;
+        this.javaMailSender = javaMailSender;
     }
 
 
@@ -44,6 +53,69 @@ public class MemberServiceImpl implements MemberService {
                 .build();
 
         memberRepository.save(memberEntity);
+    }
+
+    @Override
+    public String checkPhoneNumber(String phoneNumber) {
+
+        Boolean isExist = memberRepository.existsByMemberPhone(phoneNumber);
+
+        if (isExist) {
+            return "이미 가입된 전화번호입니다";
+        }
+
+        return "가입이 가능한 전화번호입니다";
+    }
+
+    @Override
+    public String sendSignupCode(String email) {
+
+        int code = (int) (Math.random() * 1000000);
+
+        String subject = "Lavanderia 이메일 인증" ;
+        String text = "Lavanderia 이메일 인증코드는 " + code + " 입니다";
+
+        redisTemplate.opsForValue().set(email,String.valueOf(code));
+
+        return sendEmailUtil(email,subject,text);
+    }
+
+
+    @Override
+    public String checkSignupCode(String email, String code) {
+
+        String getCode = (String) redisTemplate.opsForValue().get(email);
+
+        if (code.equals(getCode)) {
+            redisTemplate.delete(email);
+            return "이메일 인증 성공";
+        }
+
+        return "이메일 인증 실패";
+    }
+
+    @Override
+    public String sendEmail(String email, String subject, String text) {
+        return sendEmailUtil(email,subject,text);
+    }
+
+    public String sendEmailUtil(String email, String subject, String text) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try {
+
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(email); // 메일 수신자
+            mimeMessageHelper.setSubject(subject); // 메일 제목
+            mimeMessageHelper.setText(text); // 메일 본문 내용
+            javaMailSender.send(mimeMessage);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return "이메일 전송 실패";
+        }
+
+        return "이메일 전송 성공";
     }
 
     @Override
