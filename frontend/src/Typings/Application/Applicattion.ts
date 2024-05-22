820d9bd1fb847c9b770c2f94f96cea2e3f90092e
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-05-22 13:29:53 +0900

    💄 신청하기 UI 개발

---
+++ frontend/src/Typings/Application/Applicattion.ts
@@ -0,0 +1,8 @@
+// Application.ts (or Applicattion.ts if the name is correct)
+export interface ServiceCardProps {
+  title: string;
+  description: string;
+  details: string;
+  image?: string;
+  isHighlighted: boolean;
+}
