45a1033407b02a8b532cafd4659651892833f348
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-05-28 15:31:58 +0900

    :art: 라우트 구조 수정 헤더를 공용으로 빼고 Home을 헤더의 자식으로 삽입

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,32 +5,32 @@ import Application from "./Pages/Application/Application";
 import Premium from "./Pages/Premium/Premium";
 import Community from "./Pages/Community/Community";
 import ServiceCenter from "./Pages/ServiceCenter/ServiceCenter";
-import Home from "./Pages/Home";
+import Home from "./Pages/home/Home";  // 수정된 부분
 
-import Header from "./components/Common/Header";
-import Login from "./Pages/Auth/Login";
-import Agreement from "./Pages/Auth/Agreement";
-import { Signup } from "./Pages/Auth/Signup";
+import Login from "./Pages/auth/Login";
+import Agreement from "./Pages/auth/Agreement";
+import { Signup } from "./Pages/auth/Signup";
+import Header from "./Components/Common/Header";
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<Header />}>
-        <Route path="application" element={<Application />} />
-        <Route path="premium" element={<Premium />} />
-        <Route path="community" element={<Community />} />
-        <Route path="servicecenter" element={<ServiceCenter />} />
-      </Route>
-      <Route path="/" element={<Home />} />
+    return (
+        <Routes>
+            <Route element={<Header />}>
+                <Route path="/" element={<Home />} />
+                <Route path="application" element={<Application />} />
+                <Route path="premium" element={<Premium />} />
+                <Route path="community" element={<Community />} />
+                <Route path="servicecenter" element={<ServiceCenter />} />
+            </Route>
 
-      {/* 로그인, 회원가입 관련 */}
-      <Route path="/auth">
-        <Route path="login" element={<Login />} />
-        <Route path="agreement" element={<Agreement />} />
-        <Route path="signup" element={<Signup />} />
-      </Route>
-    </Routes>
-  );
+            {/* 로그인, 회원가입 관련 */}
+            <Route path="auth">
+                <Route path="login" element={<Login />} />
+                <Route path="agreement" element={<Agreement />} />
+                <Route path="signup" element={<Signup />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
