3c768dfb9381828635fdfd93e61198013f3c80c2
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-05-28 15:34:09 +0900

    :art: 헤더 반응형 추가 및 tailwind.config.js에 3xl 사이즈 추가 (1600px 이상)

--- frontend/src/Components/Common/Header.tsx
+++ frontend/src/Components/Common/Header.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navigation from "../Navigation/Navigation";
+import Navigation from "../navigation/Navigation";
 
 const Header = () => {
-  // Header 사이트의 제일 위 헤더 입니다.
-  return (
-    <header>
-      <Navigation />
-      <Outlet />
-    </header>
-  );
+    return (
+        <header>
+            <Navigation />
+            <Outlet />
+        </header>
+    );
 };
 
 export default Header;
