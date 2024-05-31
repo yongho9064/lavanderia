e6084ebe87f37a32171fccc5a46691a100b2c5a3
yongho9064 <dydgmldydgh@naver.com>
2024-05-31 18:32:51 +0900

    refactor: auth 모듈 개선

--- src/main/java/com/kyungmin/lavanderia/member/data/entity/Member.java
+++ src/main/java/com/kyungmin/lavanderia/member/data/entity/Member.java
@@ -1,14 +1,19 @@
 package com.kyungmin.lavanderia.member.data.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.DynamicInsert;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
@@ -30,9 +35,6 @@ public class Member implements UserDetails {
 
     @Column(name = "MEMBER_EMAIL")
     private String memberEmail; // 멤버 이메일
-
-    @Column(name = "MEMBER_ROLE")
-    private String memberRole; // 멤버 권한
 
     @Column(name = "MEMBER_PHONE")
     private String memberPhone; // 멤버 전화번호
@@ -70,30 +72,29 @@ public class Member implements UserDetails {
     @Column(name = "ACC_DELETE_DATE")
     private Date accDeleteDate;   // 계정 삭제 일시
 
+    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
+    List<Role> roles = new ArrayList<>();
 
     @Builder
-    public Member(String memberId, String memberPwd, String memberName, String memberEmail, String memberPhone, String agreeMarketingYn, String memberRole){
+    public Member(String memberId, String memberPwd, String memberName, String memberEmail, String memberPhone, String agreeMarketingYn, List<Role> roles){
         this.memberId = memberId;
         this.memberPwd = memberPwd;
         this.memberName = memberName;
         this.memberEmail = memberEmail;
         this.memberPhone = memberPhone;
-        this.memberRole = memberRole;
         this.agreeMarketingYn = agreeMarketingYn;
+        this.roles = roles;
     }
-
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collection = new ArrayList<>();
 
-        collection.add(new GrantedAuthority() {
-            @Override
-            public String getAuthority() {
-                return memberRole;
-            }
-        });
-        return collection;
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getAuthorities()));
+        }
+         return authorities;
     }
 
     @Override
