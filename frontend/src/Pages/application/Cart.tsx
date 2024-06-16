090496525cd7009080155ead65f5f158b6e86f01
(Leekwanyong) <(dlrkdid12@gmail.com)>
2024-06-16 23:30:03 +0900

    feat: Add 장바구니UI 수정

--- frontend/src/Pages/application/Cart.tsx
+++ frontend/src/Pages/application/Cart.tsx
@@ -114,8 +114,8 @@ const Cart: React.FC = () => {
                     <p key={item.name} className="text-sm text-gray-500 mb-1 font-bold">{item.name} x {item.quantity}</p>
                 ))}
                 <div className="flex justify-between items-center mt-3">
-                  <p className="text-xl font-bold">총 결제금액: </p>
-                  <p className="text-xl font-bold">{totalAmount.toLocaleString()}원</p>
+                  <p className=" font-bold">총 결제금액: </p>
+                  <p className=" font-bold">{totalAmount.toLocaleString()}원</p>
                 </div>
               </div>
             </div>
