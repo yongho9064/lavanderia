a6fca3fec006a56584bac18d50c97b04a3476981
yongho9064 <dydgmldydgh@naver.com>
2024-10-08 14:36:08 +0900

    fix: cors 문제 해결
    - SecurityConfig 클래스의 filterChain 메소드의 리턴값이 null로 되어있었음

--- src/main/java/com/kyungmin/lavanderia/global/config/CorsMvcConfig.java
+++ src/main/java/com/kyungmin/lavanderia/global/config/CorsMvcConfig.java
@@ -1,17 +1,19 @@
 package com.kyungmin.lavanderia.global.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
 
-    @Override
+    /*@Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                .exposedHeaders("Set-Cookie")
-                .allowedOrigins("http://localhost:3000");
-    }
+         corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // 허용할 출처
+                .allowedMethods("*") // 모든 HTTP 메서드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true) // 자격 증명 허용
+                .exposedHeaders("Set-Cookie", "Authorization"); // 'Set-Cookie' 및 'Authorization' 헤더 노출
+    }*/
 
 }
