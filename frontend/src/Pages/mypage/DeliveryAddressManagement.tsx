ec876baeeeeeecda754e9a18da8d9abd57c404ac
Leekwanyong <dlrkdid12@gmail.com>
2024-10-17 12:18:37 +0900

    feat: 회원가입 페이지 router 설정

---
+++ frontend/src/Pages/mypage/DeliveryAddressManagement.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EnterPassword from "./EnterPassword";
+
+const DeliveryAddressManagement = () => {
+    return (
+        <div>
+            <EnterPassword/>
+        </div>
+    );
+};
+
+export default DeliveryAddressManagement;
---
+++ frontend/src/Pages/mypage/PaymentManagement.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentManagement = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PaymentManagement;
